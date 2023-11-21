db09438326342e957de155e190a03001d682de3f
Francisco <pacotecocoatzi@gmail.com>
2023-11-21 11:44:23 -0600

    update index.php

--- php/index.php
+++ php/index.php
@@ -1,1 +1,16 @@
+<?php include('header.php');?>
+<?php include('db.php');?>
 <h1>Comenzamos con estructura HTML</h1>
+<h2>aqui ira el formulario y demas, es donde trabajaran los front para la estructura html</h2>
+
+<h3>como tal este es nuestro index, por que si ultilizamos el index.html no podre manipular con php</h3>
+
+
+
+
+
+
+
+
+
+<?php include('footer');?>
